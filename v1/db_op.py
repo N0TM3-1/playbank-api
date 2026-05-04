5f9b66a80b7b12ca80c308a1cde7eb7cfa2f5e47
dumitru <copaceanudumitru0@gmail.com>
2026-05-04 23:44:49 +0300

    Setup API key generation and username validation

--- v1/db_op.py
+++ v1/db_op.py
@@ -26,8 +26,9 @@ def init():
         cur.execute('''
                     CREATE TABLE IF NOT EXISTS users (
                         id SERIAL PRIMARY KEY,
-                        username VARCHAR(20) NOT NULL,
-                        password text NOT NULL
+                        username VARCHAR(20) NOT NULL UNIQUE,
+                        email varchar(255),
+                        api_key BYTEA NOT NULL
                     )
                     ''')
     except psycopg2.Error as e:
@@ -36,12 +37,15 @@ def init():
     else:
         conn.commit()
 
-def add_user(username, password):
+def add_user(username, email, api_key):
     try:
         cur.execute(
-            '''INSERT INTO users (username, password) VALUES (%s, %s)''',
-            (username, password)
+            '''INSERT INTO users (username, email, api_key) VALUES (%s, %s, %s)''',
+            (username, email, api_key)
         )
+    except psycopg2.errors.UniqueViolation as e:
+        conn.rollback()
+        return 'unique_violation'
     except psycopg2.Error as e:
         conn.rollback()
         logging.exception('PSYCOPG2.ERROR') # TODO Fix this
--- v1/users.py
+++ v1/users.py
@@ -1,18 +1,36 @@
 from flask import Blueprint, request, jsonify
+import secrets, base64, bcrypt
 from . import db_op as db
 
 users = Blueprint('users', __name__, url_prefix='/users')
 
-@users.route('/', methods=['POST'])
+def gen_key(): # Generate API key
+    key = secrets.token_bytes(32) # Generate random key
+    user_key = base64.urlsafe_b64encode(key).decode('utf-8') # Encode to create the raw API key
+    db_key = bcrypt.hashpw(user_key.encode(), bcrypt.gensalt()) # Salt and hash the API key for storing
+    return user_key, db_key
+
+def check_user(username, email): # TODO Setup email validation
+    if not username or not username.strip():
+        return False, jsonify({'error':'validation_error', 'message':'Username is required and can not be empty'}), 400
+    if ' ' in username:
+        return False, jsonify({'message':'Username must not contain spaces'}), 400
+    return True
+
+@users.route('', methods=['POST'])
 def create_user():
-    data = request.get_json()
+    data = request.get_json(silent=True)
     if data:
-        username=data.get('username')
-        password=data.get('password')
-        res = db.add_user(username, password)
+        username=data.get('username') # Used for identification
+        email = data.get('email') # Optional
+        check_user(username, email)
+        api_key, db_key = gen_key()
+        res = db.add_user(username, email, db_key)
         if res==True:
-            return jsonify({'message':'User created successfuly'}), 201
+            return jsonify({'api_key':f'{api_key}'}), 201
+        elif res == 'unique_violation':
+            return jsonify({'error':'conflict', 'message':f'User {username} already exists'}), 409
         else:
-            return jsonify({'message':'Internal Server Error'}), 500
+            return jsonify({'error':'database_error', 'message':'Database operation failes'}), 500
     else:
-        return jsonify({'error':'Bad Request', 'message':'No content received'}), 400
+        return jsonify({'error':'bad_request', 'message':'Missing or invalid JSON'}), 400
