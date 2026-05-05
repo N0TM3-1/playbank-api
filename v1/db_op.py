06d73811d278582e912e11cd5ce2568711762e9f
dumitru <copaceanudumitru0@gmail.com>
2026-05-05 23:18:56 +0300

    Refactored database connection handling

--- v1/db_op.py
+++ v1/db_op.py
@@ -1,7 +1,5 @@
-import psycopg2
-import os
+import psycopg2, os, logging
 from dotenv import load_dotenv
-import logging
 
 load_dotenv()
 
@@ -11,17 +9,24 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 
-conn = psycopg2.connect(
-    host = DB_HOST,
-    port=DB_PORT,
-    database = DB_NAME,
-    user = DB_USER,
-    password = DB_PASS
-)
+def connect():
+    conn = psycopg2.connect(
+        host = DB_HOST,
+        port=DB_PORT,
+        database = DB_NAME,
+        user = DB_USER,
+        password = DB_PASS
+    )
 
-cur = conn.cursor()
+    cur = conn.cursor()
+    return conn, cur
+
+def disconnect(conn, cur):
+    cur.close()
+    conn.close()
 
 def init():
+    conn, cur = connect()
     try:
         cur.execute('''
                     CREATE TABLE IF NOT EXISTS users (
@@ -34,10 +39,13 @@ def init():
     except psycopg2.Error as e:
         conn.rollback()
         logging.exception('PSYCOPG2.ERROR') # TODO Fix this
+        disconnect(conn, cur)
     else:
         conn.commit()
+        disconnect(conn, cur)
 
 def add_user(username, email, api_key):
+    conn, cur = connect()
     try:
         cur.execute(
             '''INSERT INTO users (username, email, api_key) VALUES (%s, %s, %s)''',
@@ -45,11 +53,14 @@ def add_user(username, email, api_key):
         )
     except psycopg2.errors.UniqueViolation as e:
         conn.rollback()
+        disconnect(conn, cur)
         return 'unique_violation'
     except psycopg2.Error as e:
         conn.rollback()
         logging.exception('PSYCOPG2.ERROR') # TODO Fix this
+        disconnect(conn, cur)
         return e
     else:
         conn.commit()
+        disconnect(conn, cur)
         return True
